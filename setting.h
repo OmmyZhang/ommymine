aed3056f7be1997f267ce9c921ca2b86e45d2cab
ommyzhang <tdxdxoz@gmail.com>
2016-08-25 02:59:35 +0800

    忽略掉moc_文件_成功

---
+++ setting.h
@@ -0,0 +1,26 @@
+#ifndef _SET_H_
+#define _SET_H_
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <QApplication>
+#include <QLabel>
+#include <QDialog>
+#include <QLineEdit>
+#include <QIntValidator>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QLayout>
+#include <QSignalMapper>
+#include <QScrollBar>
+#include <QMovie>
+#include <QMessageBox>
+#include <QMouseEvent>
+
+#define CON QObject::connect 
+#define DISCON QObject::disconnect 
+#define MAXN 21
+#define MAXM 21
+
+#endif
